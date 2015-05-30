df6a103646a973609633b3b6f915ea3173ff10c7
cactus512 <dhaka96512@gmail.com>
2015-05-30 13:32:02 +0000

    to add date search feature

--- app/models/invoice_search.rb
+++ app/models/invoice_search.rb
@@ -3,8 +3,8 @@ class InvoiceSearch
     
     def initialize(params)
         params ||= {}
-        @date_from= Parsed_date(params[date_from], 7.days.ago.to_date.to_s )
-        @date_to= Parsed_date(params[date_to], Date.today.to_s )
+        @date_from = parsed_date(params[date_from], 7.days.ago.to_date.to_s )
+        @date_to= parsed_date(params[date_to], Date.today.to_s )
     end
     
     def scope
@@ -14,7 +14,7 @@ class InvoiceSearch
     private 
     
     def parsed_date (date_string,default)
-        date.parse(date_string)
+        Date.parse(date_string)
     rescue ArgumentError , TypeError
     default
     end
